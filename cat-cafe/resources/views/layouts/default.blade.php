dd371e2e921b6ff9eb0e6352e02f8033e05cdb45
hidepon503 <blue.passion625@gmail.com>
2023-08-16 18:07:26 +0900

    タイトル変更

--- cat-cafe/resources/views/layouts/default.blade.php
+++ cat-cafe/resources/views/layouts/default.blade.php
@@ -16,7 +16,7 @@
 <header>
     <div class="container px-4 mx-auto">
         <nav class="flex items-center justify-between py-6">
-            <a class="text-3xl font-semibold leading-none" href="/">ねこカフェららべる</a>
+            <a class="text-3xl font-semibold leading-none" href="/">WithPets</a>
             <ul class="hidden lg:flex ml-12 mr-auto space-x-12">
                 <li><a class="text-sm text-blueGray-400 hover:text-blueGray-500" href="#">設備</a></li>
                 <li><a class="text-sm text-blueGray-400 hover:text-blueGray-500" href="#">ねこちゃんたち</a></li>
@@ -43,7 +43,7 @@
 <footer class="bg-black">
     <div class="px-4 container mx-auto p-10 flex justify-between">
         <div class="text-white text-left">
-            <h2 class="text-xl font-semibold">ねこカフェららべる</h2>
+            <h2 class="text-xl font-semibold">WithPets</h2>
             <p>〒123-4567</p>
             <p>東京都墨田区押上1-2-3 Illuminateビル9F</p>
         </div>
